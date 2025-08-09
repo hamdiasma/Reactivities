e239c2878645c4d828a227d330655166ac9e1421
hamdiasma <hamdi.babdelhafidh@gmail.com>
2025-08-09 10:43:03 +0200

    install react project

---
+++ Client/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:5000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    // },
+  },
+  // build: {
+  //   outDir: '../wwwroot',
+  // },
+  plugins: [react()],
+})
